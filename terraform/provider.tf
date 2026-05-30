6d4e0e905b288aae1c36f486955770e7534995ab
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-30 21:40:58 +0530

    catalogue-cd-jenkins

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "devopsprocloud-roboshop-infra-dev"
-    key = "catalogue"
+    key = "components"
     region = "us-east-1"
     encrypt = true
     use_lockfile = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,3 +35,28 @@ variable "domain_name" {
 variable "app_version" {
   
 }
+
+variable "components" {
+    default = {
+        # backend components are attaching to backend ALB
+        catalogue = {
+            rule_priority = 10
+        }
+        # user = {
+        #     rule_priority = 20
+        # }
+        # cart = {
+        #     rule_priority = 30
+        # }
+        # shipping = {
+        #     rule_priority = 40
+        # }
+        # payment = {
+        #     rule_priority = 50
+        # }
+        # # this is attaching to frontend ALB, there is only component there
+        # frontend = {
+        #     rule_priority = 10
+        # }
+    }
+}
