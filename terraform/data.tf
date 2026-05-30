67d7e7df9a0e2fc6ccb83a4b889bff0aa2a693b2
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-30 10:03:44 +0530

    catalogue-cd-jenkis

--- terraform/data.tf
+++ terraform/data.tf
@@ -17,6 +17,9 @@ data "aws_ami" "rhel-9" {
     values = ["hvm"]
   }
 }
+data "aws_ssm_parameter" "bastion_public_ip" {
+  name = "/${var.project}/${var.environment}/bastion_public_ip"
+}
 
 data "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/backend_alb_sg_id"
